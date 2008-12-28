ffd2471c304d706916c3b423ecaeada49b2a1e40
Willem van Bergen <willem@vanbergen.org>
2008-12-28 21:13:25 +0100

    Add failed requests to report

--- lib/request_log_analyzer/file_format/rails.rb
+++ lib/request_log_analyzer/file_format/rails.rb
@@ -73,7 +73,9 @@ module RequestLogAnalyzer::FileFormat::Rails
       track(:duration, :duration => :db,       :category => REQUEST_CATEGORIZER, :title => "View rendering time", :line_type => :completed)
       
       track(:category, :category => REQUEST_CATEGORIZER, :title => 'Process blockers (> 1 sec duration)', :line_type => :completed,
-              :if => lambda { |request| request[:duration] > 1.0 })
+              :if => lambda { |request| request[:duration] > 1.0 }, :amount => 20)
+              
+      track(:category, :category => :error, :title => 'Failed requests', :line_type => :failed, :amount => 20)              
     end
     
     REQUEST_CATEGORIZER = Proc.new do |request|
