35d8b1be80bd5f06d07eb4c0e0b1818c709ae962
Bart ten Brinke <info@retrosync.com>
2008-12-29 23:15:34 +0100

    The graph is back \!\!

--- lib/request_log_analyzer/aggregator/summarizer.rb
+++ lib/request_log_analyzer/aggregator/summarizer.rb
@@ -178,15 +178,17 @@ module RequestLogAnalyzer::Aggregator
 
     class TimespanTracker < Tracker
       
-      attr_reader :first, :last
+      attr_reader :first, :last, :request_time_graph
       
       def prepare
         raise "No categorizer set up for category tracker #{self.inspect}" unless options[:field]
+        @request_time_graph = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
       end
                   
       def update(request)
         timestamp = request[options[:field]]
-        
+
+        @request_time_graph[timestamp.to_s[8..9].to_i] +=1
         @first = timestamp if @first.nil? || timestamp < @first
         @last  = timestamp if @last.nil?  || timestamp > @last
       end
@@ -199,11 +201,34 @@ module RequestLogAnalyzer::Aggregator
         
         first_date  = DateTime.parse(@first.to_s, '%Y%m%d%H%M%S')
         last_date   = DateTime.parse(@last.to_s, '%Y%m%d%H%M%S')
+        days        = (@last && @first) ? (last_date - first_date).ceil : 1
 
         puts "First request:        #{first_date.strftime('%Y-%m-%d %H:%M:%I')}"
         puts "Last request:         #{last_date.strftime('%Y-%m-%d %H:%M:%I')}"        
-        puts "Total time analyzed:  #{(@last && @first) ? (last_date - first_date).ceil : '-1' } days"
+        puts "Total time analyzed:  #{days} days"
         puts ""
+        
+        puts "Requests graph - average per day per hour"
+        puts green("========================================================================", color)
+
+        deviation         = @request_time_graph.max / 20
+        deviation         = 1 if deviation == 0
+        color_cutoff      = 15
+          
+        @request_time_graph.each_with_index do |requests, index|
+          display_chars = requests / deviation
+          request_today = requests / days
+          
+          if display_chars >= color_cutoff
+            display_chars_string = green((' ΢' * color_cutoff), color) + red((' ΢' * (display_chars - color_cutoff)), color)
+          else
+            display_chars_string = green((' ΢' * display_chars), color)
+          end
+          
+          puts "#{index.to_s.rjust(10)}:00 - #{(request_today.to_s + ' hits').ljust(15)} : #{display_chars_string}"
+        end
+        puts ""
+
       end
     end
     
