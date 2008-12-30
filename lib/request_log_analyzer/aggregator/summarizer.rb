3e4906eae84c946c57390b75f21e50085330ad65
Bart ten Brinke <info@retrosync.com>
2008-12-30 15:10:51 +0100

    Seperated hourly spread into new tracker

--- lib/request_log_analyzer/aggregator/summarizer.rb
+++ lib/request_log_analyzer/aggregator/summarizer.rb
@@ -182,13 +182,11 @@ module RequestLogAnalyzer::Aggregator
       
       def prepare
         raise "No categorizer set up for category tracker #{self.inspect}" unless options[:field]
-        @request_time_graph = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
       end
                   
       def update(request)
         timestamp = request[options[:field]]
 
-        @request_time_graph[timestamp.to_s[8..9].to_i] +=1
         @first = timestamp if @first.nil? || timestamp < @first
         @last  = timestamp if @last.nil?  || timestamp > @last
       end
@@ -207,14 +205,37 @@ module RequestLogAnalyzer::Aggregator
         puts "Last request:         #{last_date.strftime('%Y-%m-%d %H:%M:%I')}"        
         puts "Total time analyzed:  #{days} days"
         puts ""
+      end
+    end
 
-        
+    class HourlyspreadTracker < Tracker
+      
+      attr_reader :first, :last, :request_time_graph
+      
+      def prepare
+        raise "No categorizer set up for category tracker #{self.inspect}" unless options[:field]
+        @request_time_graph = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+      end
+                  
+      def update(request)
+        timestamp = request[options[:field]]
+
+        @request_time_graph[timestamp.to_s[8..9].to_i] +=1
+        @first = timestamp if @first.nil? || timestamp < @first
+        @last  = timestamp if @last.nil?  || timestamp > @last
+      end
+      
+      def report(color = false)
+        puts ""
         puts "Requests graph - average per day per hour"
-        puts green("=========================================", color)
+        puts green("======================================================================", color)
 
-        deviation         = @request_time_graph.max / 20
-        deviation         = 1 if deviation == 0
-        color_cutoff      = 15
+        first_date    = DateTime.parse(@first.to_s, '%Y%m%d%H%M%S')
+        last_date     = DateTime.parse(@last.to_s, '%Y%m%d%H%M%S')
+        days          = (@last && @first) ? (last_date - first_date).ceil : 1
+        deviation     = @request_time_graph.max / 20
+        deviation     = 1 if deviation == 0
+        color_cutoff  = 15
           
         @request_time_graph.each_with_index do |requests, index|
           display_chars = requests / deviation
@@ -226,7 +247,7 @@ module RequestLogAnalyzer::Aggregator
             display_chars_string = green((' ΢' * display_chars), color)
           end
           
-          puts "#{index.to_s.rjust(10)}:00 - #{(request_today.to_s + ' hits').ljust(15)} : #{display_chars_string}"
+          puts "#{index.to_s.rjust(3)}:00 - #{(request_today.to_s + ' hits').ljust(15)} : #{display_chars_string}"
         end
         puts ""
 
