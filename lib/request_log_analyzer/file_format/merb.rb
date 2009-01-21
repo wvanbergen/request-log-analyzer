ddaf03086c1724e28d19badfe5ff9779fd206820
Willem van Bergen <willem@vanbergen.org>
2009-01-21 06:59:37 +0100

    Basic Merb format file

---
+++ lib/request_log_analyzer/file_format/merb.rb
@@ -0,0 +1,39 @@
+module RequestLogAnalyzer::FileFormat
+  
+  class Merb < Base
+
+    # ~ Started request handling: Fri Aug 29 11:10:23 +0200 2008
+    line_definition :started do |line|
+      line.header = true
+      line.teaser = /Started/
+      line.regexp = /Started request handling\:\ (.+)/
+      line.captures << { :name => :timestamp, :type => :timestamp, :anonymize => :slightly }
+    end    
+    
+    # ~ Params: {"action"=>"create", "controller"=>"session"}
+    # ~ Params: {"_method"=>"delete", "authenticity_token"=>"[FILTERED]", "action"=>"d}
+    line_definition :params do |line|
+      line.teaser = /Params/
+      line.regexp = /Params\:\ \{(.+)\}/
+      line.captures << { :name => :raw_hash, :type => :string}
+    end
+
+    # ~ {:dispatch_time=>0.006117, :after_filters_time=>6.1e-05, :before_filters_time=>0.000712, :action_time=>0.005833}
+    line_definition :completed do |line|
+      line.footer = true
+      line.teaser = /\{:dispatch_time/
+      line.regexp = /\{\:dispatch_time=>(\d+\.\d+(?:e-?\d+)?), (?:\:after_filters_time=>(\d+\.\d+(?:e-?\d+)?), )?(?:\:before_filters_time=>(\d+\.\d+(?:e-?\d+)?), )?\:action_time=>(\d+\.\d+(?:e-?\d+)?)\}/
+      line.captures << { :name => :dispatch_time,       :type => :sec, :anonymize => :slightly } \
+                    << { :name => :after_filters_time,  :type => :sec, :anonymize => :slightly } \
+                    << { :name => :before_filters_time, :type => :sec, :anonymize => :slightly } \
+                    << { :name => :action_time,         :type => :sec, :anonymize => :slightly }
+    end
+    
+    
+    report do |analyze|
+      # FIX ME
+    end
+    
+  end
+
+end
--- spec/merb_format_spec.rb
+++ spec/merb_format_spec.rb
@@ -1,38 +1,38 @@
-# require File.dirname(__FILE__) + '/spec_helper'
-# 
-# describe RequestLogAnalyzer::Source::LogParser, "Merb" do
-#   include RequestLogAnalyzerSpecHelper
-#   
-#   before(:each) do
-#     @log_parser = RequestLogAnalyzer::Source::LogParser.new(:merb)
-#   end
-#   
-#   it "should have a valid language definitions" do
-#     @log_parser.file_format.should be_valid
-#   end
-#   
-#   it "should parse a stream and find valid requests" do
-#     File.open(log_fixture(:merb), 'r') do |io|
-#       @log_parser.parse_io(io) do |request| 
-#         request.should be_kind_of(RequestLogAnalyzer::Request)
-#       end
-#     end
-#   end
-#
-#   it "should find 11 completed requests" do
-#     @log_parser.should_receive(:handle_request).exactly(11).times
-#     @log_parser.parse_file(log_fixture(:merb))
-#   end
-#   
-#   it "should parse all details from a request correctly" do
-#     request = nil
-#     @log_parser.parse_file(log_fixture(:merb)) { |found_request| request ||= found_request }
-#     
-#     request.should be_completed
-#     request[:timestamp].should == DateTime.parse('Fri Aug 29 11:10:23 +0200 2008')
-#     request[:dispatch_time].should == 0.243424
-#     request[:after_filters_time].should == 6.9e-05
-#     request[:before_filters_time].should == 0.213213
-#     request[:action_time].should == 0.241652
-#   end
-# end
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe RequestLogAnalyzer::Source::LogParser, :merb do
+  include RequestLogAnalyzerSpecHelper
+  
+  before(:each) do
+    @log_parser = RequestLogAnalyzer::Source::LogParser.new(RequestLogAnalyzer::FileFormat.load(:merb))
+  end
+  
+  it "should have a valid language definitions" do
+    @log_parser.file_format.should be_valid
+  end
+  
+  it "should parse a stream and find valid requests" do
+    File.open(log_fixture(:merb), 'r') do |io|
+      @log_parser.parse_io(io) do |request| 
+        request.should be_kind_of(RequestLogAnalyzer::Request)
+      end
+    end
+  end
+
+  it "should find 11 completed requests" do
+    @log_parser.should_receive(:handle_request).exactly(11).times
+    @log_parser.parse_file(log_fixture(:merb))
+  end
+  
+  it "should parse all details from a request correctly" do
+    request = nil
+    @log_parser.parse_file(log_fixture(:merb)) { |found_request| request ||= found_request }
+    
+    request.should be_completed
+    #request[:timestamp].should == DateTime.parse('Fri Aug 29 11:10:23 +0200 2008') # FIX ME
+    request[:dispatch_time].should == 0.243424
+    request[:after_filters_time].should == 6.9e-05
+    request[:before_filters_time].should == 0.213213
+    request[:action_time].should == 0.241652
+  end
+end
