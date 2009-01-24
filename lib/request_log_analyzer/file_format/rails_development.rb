4b1bc7b9f1cdaaef4fd9cf9ff1dde687ac4c0356
Willem van Bergen <willem@vanbergen.org>
2009-01-24 09:29:18 +0100

    Improved RailsDevelopment file format

--- lib/request_log_analyzer/file_format/rails_development.rb
+++ lib/request_log_analyzer/file_format/rails_development.rb
@@ -13,28 +13,33 @@ module RequestLogAnalyzer::FileFormat
   
     #  [4;36;1mUser Load (0.4ms)[0m   [0;1mSELECT * FROM `users` WHERE (`users`.`id` = 18205844) [0m
     line_definition :query_executed do |line|
-      line.regexp = /\s+(?:\e\[4;36;1m)?((?:\w+::)*\w+) Load \((\d+\.\d+)ms\)(?:\e\[0m)?\s+(?:\e\[0;1m)?(.+) (?:\e\[0m)?/
+      line.regexp = /\s+(?:\e\[4;36;1m)?((?:\w+::)*\w+) Load \((\d+\.\d+)ms\)(?:\e\[0m)?\s+(?:\e\[0;1m)?([^\e]+) ?(?:\e\[0m)?/
       line.captures << { :name => :query_class,    :type  => :string } \
                     << { :name => :query_duration, :type  => :duration, :unit => :msec } \
-                    << { :name => :query_sql,      :type  => :string }
+                    << { :name => :query_sql,      :type  => :sql }
     end
 
     #  [4;35;1mCACHE (0.0ms)[0m   [0mSELECT * FROM `users` WHERE (`users`.`id` = 0) [0m  
     line_definition :query_cached do |line|
-      line.teaser = /\s+(?:\e\[4;35;1m)?CACHE \((\d+\.\d+)ms\)(?:\e\[0m)?\s+(?:\e\[0m)?(.+) (?:\e\[0m)?/  
-      line.regexp = /\s+(?:\e\[4;35;1m)?CACHE \((\d+\.\d+)ms\)(?:\e\[0m)?\s+(?:\e\[0m)?(.+) (?:\e\[0m)?/
+      line.regexp = /\s+(?:\e\[4;35;1m)?CACHE \((\d+\.\d+)ms\)(?:\e\[0m)?\s+(?:\e\[0m)?([^\e]+) ?(?:\e\[0m)?/
       line.captures << { :name => :cached_duration, :type  => :duration, :unit => :msec } \
-                    << { :name => :cached_sql,      :type  => :string }
+                    << { :name => :cached_sql,      :type  => :sql }
     end  
-
 
     report(:append) do |analyze|
       analyze.duration :render_duration, :category => :render_file, :multiple_per_request => true, 
               :amount => 20, :title => 'Partial rendering duration'
               
-      analyze.duration :query_duration, :category => :query_class, :multiple_per_request => true, 
+      analyze.duration :query_duration, :category => :query_sql, :multiple_per_request => true, 
               :amount => 20, :title => 'Query duration per model'
               
     end  
+    
+    
+    class Request < Rails::Request
+      def convert_sql(sql, definition) 
+        sql.gsub(/\b\d+\b/, ':id').gsub(/'[^']*'/, ':string')
+      end
+    end
   end
 end
