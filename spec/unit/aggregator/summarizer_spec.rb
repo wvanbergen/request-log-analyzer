9bec38dbd3802e5ff75d1e3e1ba7049ce4daf4d2
Willem van Bergen <willem@vanbergen.org>
2009-01-27 06:22:16 +0100

    Basic summarizer spec added to test reporting

--- spec/unit/aggregator/summarizer_spec.rb
+++ spec/unit/aggregator/summarizer_spec.rb
@@ -1,5 +1,28 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe RequestLogAnalyzer::Aggregator::Summarizer do
+  
+  include RequestLogAnalyzer::Spec::Helper
+
+  before(:each) do
+    @summarizer = RequestLogAnalyzer::Aggregator::Summarizer.new(mock_source, :output => mock_output)
+    @summarizer.prepare
+  end
+  
+  it "not raise exception when creating a report after aggregating multiple requests" do
+    @summarizer.aggregate(request(:data => 'bluh1'))
+    @summarizer.aggregate(request(:data => 'bluh2'))    
+    
+    lambda { @summarizer.report(mock_output) }.should_not raise_error
+  end
+
+  it "not raise exception when creating a report after aggregating a single request" do
+    @summarizer.aggregate(request(:data => 'bluh1'))
+    lambda { @summarizer.report(mock_output) }.should_not raise_error
+  end
+
+  it "not raise exception when creating a report after aggregating no requests" do
+    lambda { @summarizer.report(mock_output) }.should_not raise_error
+  end
 
 end
