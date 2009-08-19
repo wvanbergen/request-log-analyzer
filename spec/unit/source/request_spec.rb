b6d4bf996be77dff469ed6c8edc27acadb6c082a
Willem van Bergen <willem@vanbergen.org>
2009-08-19 21:41:08 +0200

    Fixed first_lineno and last_lineno to not depend on the correct ordering of lines

--- spec/unit/source/request_spec.rb
+++ spec/unit/source/request_spec.rb
@@ -1,85 +1,111 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe RequestLogAnalyzer::Request, :incomplete_request do
+describe RequestLogAnalyzer::Request do
   
   before(:each) do
-    @incomplete_request = testing_format.request
-    @incomplete_request << { :line_type => :test, :lineno => 1, :test_capture => 'awesome!' }
-  end
-  
-  it "should be single if only one line has been added" do
-    @incomplete_request.should_not be_empty
-  end
-  
-  it "should not be a completed request" do
-    @incomplete_request.should_not be_completed
+    @request = testing_format.request
   end  
   
-  it "should take the line type of the first line as global line_type" do
-    @incomplete_request.lines[0][:line_type].should == :test
-    @incomplete_request.should =~ :test
+  it "should be empty without any captured lines in it" do
+    @request.should be_empty
   end
   
-  it "should return the first field value" do
-    @incomplete_request[:test_capture].should == 'awesome!'
+  context :incomplete do
+  
+    before(:each) do
+      @request << { :line_type => :test, :lineno => 1, :test_capture => 'awesome!' }
+    end
+  
+    it "should be single if only one line has been added" do
+      @request.should_not be_empty
+    end
+  
+    it "should not be a completed request" do
+      @request.should_not be_completed
+    end  
+  
+    it "should take the line type of the first line as global line_type" do
+      @request.lines[0][:line_type].should == :test
+      @request.should =~ :test
+    end
+  
+    it "should return the first field value" do
+      @request[:test_capture].should == 'awesome!'
+    end
+  
+    it "should return nil if no such field is present" do
+      @request[:nonexisting].should be_nil
+    end
   end
   
-  it "should return nil if no such field is present" do
-    @incomplete_request[:nonexisting].should be_nil
+  context :completed do
+    
+    before(:each) do
+      @request << { :line_type => :first, :lineno =>  1, :name => 'first line!' }    
+      @request << { :line_type => :test,  :lineno =>  4, :test_capture => 'testing' }        
+      @request << { :line_type => :test,  :lineno =>  7, :test_capture => 'testing some more' }            
+      @request << { :line_type => :last,  :lineno => 10, :time => 0.03 }
+    end
+
+    it "should not be empty when multiple liness are added" do
+      @request.should_not be_empty
+    end
+
+    it "should be a completed request" do
+      @request.should be_completed
+    end
+
+    it "should recognize all line types" do
+      [:first, :test, :last].each { |type| @request.should =~ type }
+    end
+
+    it "should detect the correct field value" do
+      @request[:name].should == 'first line!'
+      @request[:time].should == 0.03
+    end
+
+    it "should detect the first matching field value" do  
+      @request.first(:test_capture).should == 'testing'
+    end
+
+    it "should detect the every matching field value" do  
+      @request.every(:test_capture).should == ['testing', "testing some more"]
+    end
+
+    it "should set the first_lineno for a request to the lowest lineno encountered" do
+      @request.first_lineno.should eql(1)
+    end
+    
+    it "should set the first_lineno for a request if a line with a lower lineno is added" do
+      @request << { :line_type => :test, :lineno =>  0 }          
+      @request.first_lineno.should eql(0)
+    end    
+
+    it "should set the last_lineno for a request to the highest encountered lineno" do
+      @request.last_lineno.should eql(10)
+    end
+    
+    it "should not set the last_lineno for a request if a line with a lower lineno is added" do
+      @request << { :line_type => :test, :lineno =>  7 }          
+      @request.last_lineno.should eql(10)
+    end
+
+    it "should not have a timestamp if no such field is captured" do
+      @request.timestamp.should be_nil
+    end
+
+    it "should set return a timestamp field if such a field is captured" do
+      @request << { :line_type => :first, :lineno =>  1, :name => 'first line!', :timestamp => Time.now}    
+      @request.timestamp.should_not be_nil
+    end    
+  end
+  
+  context 'single line' do
+    # combined is both a header and a footer line
+    before(:each) { @request << { :line_type => :combined, :lineno => 1 } }
+  
+    it "should be a completed request if the line is both header and footer" do
+      @request.should be_completed
+    end   
   end
 end  
-
-
-describe RequestLogAnalyzer::Request, :completed_request do
-  
-  before(:each) do
-    @completed_request = testing_format.request
-    @completed_request << { :line_type => :first, :lineno =>  1, :name => 'first line!' }    
-    @completed_request << { :line_type => :test,  :lineno =>  4, :test_capture => 'testing' }        
-    @completed_request << { :line_type => :test,  :lineno =>  7, :test_capture => 'testing some more' }            
-    @completed_request << { :line_type => :last,  :lineno => 10, :time => 0.03 }
-  end
-  
-  it "should not be empty when multiple liness are added" do
-    @completed_request.should_not be_empty
-  end
-  
-  it "should be a completed request" do
-    @completed_request.should be_completed
-  end
-  
-  it "should recognize all line types" do
-    [:first, :test, :last].each { |type| @completed_request.should =~ type }
-  end
-  
-  it "should detect the correct field value" do
-    @completed_request[:name].should == 'first line!'
-    @completed_request[:time].should == 0.03
-  end
-  
-  it "should detect the first matching field value" do  
-    @completed_request.first(:test_capture).should == 'testing'
-  end
-  
-  it "should detect the every matching field value" do  
-    @completed_request.every(:test_capture).should == ['testing', "testing some more"]
-  end
-  
-  it "should set the first_lineno for a request to the lowest lineno encountered" do
-    @completed_request.first_lineno.should eql(1)
-  end
-
-  it "should set the last_lineno for a request to the highest encountered lineno" do
-    @completed_request.last_lineno.should eql(10)
-  end
-
-  it "should not have a timestamp if no such field is captured" do
-    @completed_request.timestamp.should be_nil
-  end
-
-  it "should set return a timestamp field if such a field is captured" do
-    @completed_request << { :line_type => :first, :lineno =>  1, :name => 'first line!', :timestamp => Time.now}    
-    @completed_request.timestamp.should_not be_nil
-  end
-  
-end
