cdfc814c4ef18c9947842f1ee32c4d9ee88d3351
Willem van Bergen <willem@vanbergen.org>
2009-11-13 07:24:33 +0100

    Improved and bugfixed the MySQL slow query log file format.
    * SQL queries that span over multiple lines are combined correctly, so the preprocessor is no longer needed.
    * Parse "use database;" lines separately.
    * Fixed a bug in the user@host line parser.
    * There is a patch for mysql to report query duration as a float instead of an integer in the slow query log. The regexp for this line is updated to parse these float values as well.
    * Improved rewriting of SQL queries to categorize even better and get more readable results.
    * Fixed a bug in the timespan converter method.
    * Added a timespan tracker to the report.

--- lib/request_log_analyzer/file_format/mysql.rb
+++ lib/request_log_analyzer/file_format/mysql.rb
@@ -1,56 +1,87 @@
 module RequestLogAnalyzer::FileFormat
 
   class Mysql < Base
+    
     line_definition :time do |line|
+      line.teaser = /\# Time: /
       line.regexp = /\# Time: (\d\d\d\d\d\d \d{1,2}:\d\d:\d\d)/
       line.captures << { :name => :timestamp, :type => :timestamp }
       line.header = true
     end
 
-    line_definition :meta_info do |line|
-      line.regexp = /\# User\@Host\: (\w+)\[(.*?)\].*/
+    line_definition :user_host do |line|
+      line.teaser = /\# User\@Host\: /
+      line.regexp = /\# User\@Host\: (\w+)\[\w+\] \@ ([\w\.-]+) \[([\d\.]*)\]/
       line.captures << { :name => :user, :type => :string } << 
-                       { :name => :host, :type => :string }
+                       { :name => :host, :type => :string } <<
+                       { :name => :ip,   :type => :string }
     end
 
-    line_definition :query_info do |line|
-      line.regexp = /\# Query_time: (\d+)  Lock_time: (\d+)  Rows_sent: (\d+)  Rows_examined: (\d+)/
+    line_definition :query_statistics do |line|
+      line.teaser = /\# Query_time: /
+      line.regexp = /\# Query_time: (\d+(?:\.\d+)?)\s+Lock_time: (\d+(?:\.\d+)?)\s+Rows_sent: (\d+)\s+Rows_examined: (\d+)/
       line.captures << { :name => :query_time, :type => :duration, :unit => :sec } <<
-                       { :name => :lock_time, :type => :duration, :unit => :sec } <<
+                       { :name => :lock_time,  :type => :duration, :unit => :sec } <<
                        { :name => :rows_sent, :type => :integer } <<
                        { :name => :rows_examined, :type => :integer }
     end
 
+    line_definition :use_database do |line|
+      line.regexp   = /^use (\w+);\s*$/
+      line.captures << { :name => :database, :type => :string }
+    end
+
+    line_definition :query_part do |line|
+      line.regexp   = /^(?!(?:use |\# ))(.*[^;\s])\s*$/
+      line.captures << { :name => :query_fragment, :type => :string }
+    end
+
     line_definition :query do |line|
-      line.regexp = /(.*;)/
+      line.regexp = /^(?!(?:use |\# ))(.*);\s*$/
       line.captures << { :name => :query, :type => :sql }
       line.footer = true
     end
 
-    RQ = Proc.new { |request| "#{request[:user]}@#{request[:host]}: #{request[:query]}" }
-    RU = Proc.new { |request| "#{request[:user]}" }
+    PER_USER       = :user
+    PER_QUERY      = :query
+    PER_USER_QUERY = Proc.new { |request| "#{request[:user]}@#{request[:host]}: #{request[:query]}" }
 
     report do |analyze|
-      analyze.frequency :user, :title => "Users with most queries", :amount => 20
-      analyze.duration :query_time, :category => RU, :title => 'Query time per user'
-      analyze.duration :query_time, :category => RQ, :title => 'Query time', :amount => 50
-      analyze.count :category => RQ, :title => "Rows examined", :amount => 20, :field => :rows_examined
-      analyze.count :category => RQ, :title => "Rows sent", :amount => 20, :field => :rows_sent
+      analyze.timespan
+      analyze.frequency :user, :title => "Users with most queries"
+      analyze.duration :query_time, :category => PER_USER, :title => 'Query time per user'
+      analyze.duration :query_time, :category => PER_USER_QUERY, :title => 'Query time'
+      # analyze.duration :lock_time,  :category => PER_USER_QUERY, :title => 'Lock time'
+      analyze.count :category => PER_USER_QUERY, :title => "Rows examined", :field => :rows_examined
+      analyze.count :category => PER_USER_QUERY, :title => "Rows sent",     :field => :rows_sent
     end
   
     class Request < RequestLogAnalyzer::Request
-      def convert_sql(sql, definition)
-        converted = sql.gsub(/\b\d+\b/, ':int').gsub(/`([^`]+)`/, '\1').gsub(/'[^']*'/, ':string').rstrip
-        converted.gsub!(/(:int,)+:int/, ':ints')
-        converted.gsub!(/(:string,)+:string/, ':strings')
-        converted
+
+      def convert_sql(value, definition)
+
+        # Recreate the full SQL query by joining all the previous parts and this last line
+        sql = every(:query_fragment).join("\n") + value
+
+        # Sanitize an SQL query so that it can be used as a category field.
+        # sql.gsub!(/\/\*.*\*\//, '')                                       # remove comments
+        sql.gsub!(/\s+/, ' ')                                             # remove excessive whitespace
+        sql.gsub!(/`([^`]+)`/, '\1')                                      # remove quotes from field names
+        sql.gsub!(/'\d{4}-\d{2}-\d{2}'/, ':date')                         # replace dates
+        sql.gsub!(/'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}'/, ':datetime')   # replace timestamps
+        sql.gsub!(/'[^']*'/, ':string')                                   # replace strings
+        sql.gsub!(/\b\d+\b/, ':int')                                      # replace integers
+        sql.gsub!(/(:int,)+:int/, ':ints')                                # replace multiple ints by a list
+        sql.gsub!(/(:string,)+:string/, ':strings')                       # replace multiple strings by a list
+
+        return sql.rstrip
       end
-    
-      def convert_timestamp(stamp, definition)
-        all,y,m,d,h,i,s = stamp.split(/(\d\d)(\d\d)(\d\d) (\d?\d):(\d\d):(\d\d)/)
-        "20#{y}#{m}#{d}#{h}#{i}#{s}".to_i
+
+      # Convert the timestamp to an integer
+      def convert_timestamp(value, definition)
+        all,y,m,d,h,i,s = value.split(/(\d\d)(\d\d)(\d\d) (\d?\d):(\d\d):(\d\d)/)
+        ('20%s%s%s%s%s%s' % [y,m,d,h.rjust(2, '0'),i,s]).to_i
       end
     end
   end
-
 end
