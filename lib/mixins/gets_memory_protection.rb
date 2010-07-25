d8c5ff23d0c21c813ba6f89182f849aa03d89652
Bart ten Brinke <info@retrosync.com>
2010-07-25 20:25:42 +0200

    Fixed broken memory protection

--- lib/mixins/gets_memory_protection.rb
+++ lib/mixins/gets_memory_protection.rb
@@ -29,7 +29,7 @@ class File
     if @last_pos.nil? 
       seek(0, IO::SEEK_END)
       @last_pos = pos
-      seek(current_pos, IO::SEEK_SET)
+      seek(@current_pos, IO::SEEK_SET)
     end
     
     # 
