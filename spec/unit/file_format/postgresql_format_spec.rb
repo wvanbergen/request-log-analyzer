a555cb63fd94f69a0281fa56dfaa1f2ea7c37914
Willem van Bergen <willem@vanbergen.org>
2011-02-26 07:11:48 -0500

    Cleaned up PostgreSQL spec.

--- spec/unit/file_format/postgresql_format_spec.rb
+++ spec/unit/file_format/postgresql_format_spec.rb
@@ -2,36 +2,29 @@ require 'spec_helper'
 
 describe RequestLogAnalyzer::FileFormat::Postgresql do
 
-  it "should be a valid file format" do
-    RequestLogAnalyzer::FileFormat.load(:Postgresql).should be_valid
-  end
+  subject { RequestLogAnalyzer::FileFormat.load(:Postgresql) }
+  let(:log_parser)  { RequestLogAnalyzer::Source::LogParser.new(subject) }
+  
+  it { should be_valid }
 
   describe '#parse_line' do
-    before(:each) do
-      @file_format = RequestLogAnalyzer::FileFormat.load(:Postgresql)
-    end
-
     it "should parse a :query line correctly" do
       line = '2010-10-10 13:52:07 GMT [38747]: [33-1] LOG:  00000: duration: 0.710 ms  statement: SELECT * FROM "delayed_jobs"'
-      @file_format.should parse_line(line).as(:query).and_capture(:timestamp => 20101010135207, :query_fragment => 'SELECT * FROM "delayed_jobs"')
+      subject.should parse_line(line).as(:query).and_capture(:timestamp => 20101010135207, :query_fragment => 'SELECT * FROM "delayed_jobs"')
     end
     
     it "should parse a :query_fragment line correctly" do
       line = '    ("failed_at", "locked_by", "created_at", "handler", "updated_at", "priority", "run_at", "attempts", "locked_at",'
-      @file_format.should parse_line(line).as(:query_fragment).and_capture(:query_fragment => '("failed_at", "locked_by", "created_at", "handler", "updated_at", "priority", "run_at", "attempts", "locked_at",')
+      subject.should parse_line(line).as(:query_fragment).and_capture(:query_fragment => '("failed_at", "locked_by", "created_at", "handler", "updated_at", "priority", "run_at", "attempts", "locked_at",')
     end
 
     it "should parse a :query line correctly" do
       line = '2010-10-10 13:52:07 GMT [38747]: [33-1] LOG:  00000: duration: 0.710 ms  statement: SELECT * FROM "delayed_jobs"'
-      @file_format.should parse_line(line).as(:query).and_capture(:query_time => 0.710)
-    end    
+      subject.should parse_line(line).as(:query).and_capture(:query_time => 0.710)
+    end
   end
 
   describe '#parse_io' do
-    before(:each) do
-      @log_parser = RequestLogAnalyzer::Source::LogParser.new(RequestLogAnalyzer::FileFormat.load(:postgresql))
-    end
-
     it "should parse a multiline query entry correctly" do
       fixture = <<-EOS
         2010-10-10 15:00:02 GMT [38747]: [1669-1] LOG:  00000: duration: 0.195 ms  statement: INSERT INTO "delayed_jobs" ("failed_at", "locked_by", "created_at", "handler", "updated_at", "priority", "run_at", "attempts", "locked_at", "last_error") VALUES(NULL, NULL, '2010-10-10 15:00:02.159884', E'--- !ruby/object:RuntheChooChootrain {}
@@ -40,13 +33,11 @@ describe RequestLogAnalyzer::FileFormat::Postgresql do
       EOS
 
       request_counter.should_receive(:hit!).exactly(1).times
-      @log_parser.should_not_receive(:warn)
-      @log_parser.parse_io(StringIO.new(fixture)) do |request|
+      log_parser.should_not_receive(:warn)
+      log_parser.parse_io(StringIO.new(fixture)) do |request|
         request_counter.hit! if request.kind_of?(RequestLogAnalyzer::FileFormat::Postgresql::Request) && request.completed?
         request[:query].should == 'INSERT INTO delayed_jobs (failed_at, locked_by, created_at, handler, updated_at, priority, run_at, attempts, locked_at, last_error) VALUES(NULL, NULL, :string, E:string, :string, :int, :string, :int, NULL, NULL) RETURNING id'
       end
     end
-
   end
 end
-
