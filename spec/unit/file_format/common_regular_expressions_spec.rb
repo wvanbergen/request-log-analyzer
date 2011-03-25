683a4ec81d1dbcfd019b7e248501544b29b6d8e6
Willem van Bergen <willem@vanbergen.org>
2011-03-25 13:34:21 -0400

    Added new common regular expressions for hostname- and "hostname or IP" fields, making sure they also work for IPv6.

--- spec/unit/file_format/common_regular_expressions_spec.rb
+++ spec/unit/file_format/common_regular_expressions_spec.rb
@@ -4,6 +4,32 @@ describe RequestLogAnalyzer::FileFormat::CommonRegularExpressions do
   
   include RequestLogAnalyzer::FileFormat::CommonRegularExpressions
   
+  describe '.hostname' do
+    it "should parse hostnames successfully" do
+      hostname.should =~ 'railsdoctors.com'
+      hostname.should =~ 'www.rails-doctors.com'
+      hostname.should =~ 'www.rails-doctors.com'
+      hostname.should =~ 'hostname.co.uk'
+      hostname.should =~ 'localhost'
+    
+      hostname.should_not =~ '192.168.0.1'
+      hostname.should_not =~ '3ffe:1900:4545:3:200:f8ff:fe21:67cf'
+      hostname.should_not =~ 'railsdoctors.'
+    end
+  end
+  
+  describe '.hostname_or_ip_address' do
+    it "should parse either hostnames or ip addresses" do
+      hostname_or_ip_address.should =~ 'railsdoctors.com'
+      hostname_or_ip_address.should =~ 'hostname.co.uk'
+      hostname_or_ip_address.should =~ 'localhost'
+      hostname_or_ip_address.should =~ '192.168.0.1'
+      hostname_or_ip_address.should =~ '3ffe:1900:4545:3:200:f8ff:fe21:67cf'
+      
+      hostname.should_not =~ 'railsdoctors.'
+    end
+  end
+  
   describe '.timestamp' do
     
     it "should parse timestamps with a given format" do
@@ -31,6 +57,7 @@ describe RequestLogAnalyzer::FileFormat::CommonRegularExpressions do
       ip_address.should =~ '3ffe:1900:4545:3:200:f8ff:fe21:67cf'
       ip_address.should =~ '3ffe:1900:4545:3:200:f8ff:127.0.0.1'
       ip_address.should =~ '::3:200:f8ff:127.0.0.1'
+      ip_address.should =~ '0:0:0:0:0:0:0:1'
       
       ip_address.should_not =~ 'qqqq:wwww:eeee:3q:200:wf8ff:fe21:67cf'
       ip_address.should_not =~ '3ffe44:1900f:454545:3:200:f8ff:ffff:5432'
@@ -47,7 +74,6 @@ describe RequestLogAnalyzer::FileFormat::CommonRegularExpressions do
       /^#{ip_address('-')}$/.should_not =~ '--'
       /^#{ip_address('-')}$/.should_not =~ ''
     end
-    
   end
 end
 
