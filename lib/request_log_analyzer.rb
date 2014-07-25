417fe4ab9edfa5f3ff04d019be8ef6e1c2c4ad82
Bart ten Brinke <info@retrosync.com>
2014-07-25 20:46:40 +0200

    Add IneritableAttributes Class and Specs

--- lib/request_log_analyzer.rb
+++ lib/request_log_analyzer.rb
@@ -1,15 +1,15 @@
 require 'date'
 
 # RequestLogAnalyzer is the base namespace in which all functionality of RequestLogAnalyzer is implemented.
-# This module itselfs contains some functions to help with class and source file loading. The actual 
+# This module itselfs contains some functions to help with class and source file loading. The actual
 # application startup code resides in the {RequestLogAnalyzer::Controller} class.
 #
 # The {RequestLogAnalyzer::VERSION} constant can be used to determine what version of request-log-analyzer
 # is running.
 module RequestLogAnalyzer
 
-  # Convert a string/symbol in camelcase ({RequestLogAnalyzer::Controller}) to underscores 
-  # (<tt>request_log_analyzer/controller</tt>). This function can be used to load the file (using 
+  # Convert a string/symbol in camelcase ({RequestLogAnalyzer::Controller}) to underscores
+  # (<tt>request_log_analyzer/controller</tt>). This function can be used to load the file (using
   # <tt>require</tt>) in which the given constant is defined.
   #
   # @param [#to_s] str The string-like to convert in the following format: <tt>ModuleName::ClassName</tt>.
@@ -19,11 +19,11 @@ module RequestLogAnalyzer
   end
 
   # Convert a string/symbol in underscores (<tt>request_log_analyzer/controller</tt>) to camelcase
-  # ({RequestLogAnalyzer::Controller}). This can be used to find the class that is defined in a given 
+  # ({RequestLogAnalyzer::Controller}). This can be used to find the class that is defined in a given
   # filename.
   #
   # @param [#to_s] str The string-like to convert in the f`ollowing format: <tt>module_name/class_name</tt>.
-  # @return [String] The input string converted to camelcase form.  
+  # @return [String] The input string converted to camelcase form.
   def self.to_camelcase(str)
     str.to_s.gsub(/\/(.?)/) { "::" + $1.upcase }.gsub(/(^|_)(.)/) { $2.upcase }
   end
@@ -32,6 +32,7 @@ end
 require 'request_log_analyzer/version'
 require 'request_log_analyzer/controller'
 require 'request_log_analyzer/aggregator'
+require 'request_log_analyzer/class_level_inheritable_attributes'
 require 'request_log_analyzer/file_format'
 require 'request_log_analyzer/filter'
 require 'request_log_analyzer/line_definition'
---
+++ lib/request_log_analyzer/class_level_inheritable_attributes.rb
@@ -0,0 +1,25 @@
+module ClassLevelInheritableAttributes
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def inheritable_attributes(*args)
+      @inheritable_attributes ||= [:inheritable_attributes]
+      @inheritable_attributes += args
+      args.each do |arg|
+        class_eval %(
+          class << self; attr_accessor :#{arg} end
+        )
+      end
+      @inheritable_attributes
+    end
+
+    def inherited(subclass)
+      @inheritable_attributes.each do |inheritable_attribute|
+        instance_var = "@#{inheritable_attribute}"
+        subclass.instance_variable_set(instance_var, instance_variable_get(instance_var))
+      end
+    end
+  end
+end
